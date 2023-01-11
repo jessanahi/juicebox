4dced1cf0bd04a664462273ee33d2b78cfed959f
jessanahi <jess.anahi.zamora@gmail.com>
2023-01-10 23:01:38 -0600

    seeding and drop tables

--- db/index.js
+++ db/index.js
@@ -2,3 +2,14 @@ const { Client } = require('pg');
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 module.exports = { client, }
+
+async function getAllUsers() {
+    const { rows } = await client.query(`SELECT id, username FROM users;`);
+
+    return rows;
+}
+
+module.exports = {
+    client,
+    getAllUsers,
+}
--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,47 @@
-const { client } = require('./index');
+const { client, getAllUsers } = require('./index');
+
+async function dropTables() {
+    try {
+      await client.query(`
+      DROP TABLE IF EXISTS users;
+      `);
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  async function createTables() {
+    try {
+      await client.query(`
+  
+      `);
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      client.end();
+    }
+  };
+  
+rebuildDB();
 
 async function testDB() {
     try {
         client.connect();
 
-        const result = await client.query(`SELECT * FROM users;`);
+        const { rows } = await client.query(`SELECT * FROM users;`);
 
-        console.log(result);
+        console.log(rows);
     } catch (error) {
         console.error(error);
     } finally {
