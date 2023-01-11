2a89fd53996c843e374e055266c09a60cacb36d8
jessanahi <jess.anahi.zamora@gmail.com>
2023-01-10 23:24:39 -0600

    cannot reuse a client error...

--- db/seed.js
+++ db/seed.js
@@ -1,52 +1,65 @@
 const { client, getAllUsers } = require('./index');
 
 async function dropTables() {
-    try {
-      await client.query(`
+  try {
+    console.log('Starting to drop tables...');
+
+    await client.query(`
       DROP TABLE IF EXISTS users;
       `);
-    } catch (error) {
-      throw error;
-    }
+
+    console.log('Finished dropping tables!');
+  } catch (error) {
+    console.error('Error dropping tables.');
+    throw error;
   }
-  
-  async function createTables() {
-    try {
-      await client.query(`
-  
+}
+
+async function createTables() {
+  try {
+    console.log('Starting to build tables...');
+
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+        );
       `);
-    } catch (error) {
-      throw error;
-    }
-  };
-  
+
+    console.log('Finished building tables!');
+  } catch (error) {
+    console.error('Error building tables.');
+    throw error;
+  }
+}
+
 async function rebuildDB() {
-    try {
-      client.connect();
-  
-      await dropTables();
-      await createTables();
-    } catch (error) {
-      console.error(error);
-    } finally {
-      client.end();
-    }
-  };
-  
-rebuildDB();
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function testDB() {
-    try {
-        client.connect();
+  try {
+    client.connect();
 
-        const { rows } = await client.query(`SELECT * FROM users;`);
+    const { rows } = await client.query(`SELECT * FROM users;`);
 
-        console.log(rows);
-    } catch (error) {
-        console.error(error);
-    } finally {
-        client.end();
-    }
-};
+    console.log(rows);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
 
-testDB();
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
